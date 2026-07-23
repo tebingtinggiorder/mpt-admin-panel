428eebaafda19d2c3e298bb76c093050b36e1c5b
tebingtinggiorder <ferdiansyahadil51@gmail.com>
2026-07-23 15:25:41 +0700

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,12 +1,12 @@
 // firebase-init.js - Konfigurasi Utama Firebase SDK Compat
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    databaseURL: "YOUR_DATABASE_URL",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "MASUKKAN_API_KEY_ANDA",
+    authDomain: "MASUKKAN_AUTH_DOMAIN_ANDA",
+    databaseURL: "https://tebingtinggiorder-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "MASUKKAN_PROJECT_ID_ANDA",
+    storageBucket: "MASUKKAN_STORAGE_BUCKET_ANDA",
+    messagingSenderId: "MASUKKAN_MESSAGING_SENDER_ID_ANDA",
+    appId: "MASUKKAN_APP_ID_ANDA"
 };
 
 // Inisialisasi Firebase
